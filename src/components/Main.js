29f338eb2708da73a84f1b545caf8aabafd46d71
myilmaz <myilmaz@subilgi.com.tr>
2021-07-30 14:05:57 +0300

    routing from different page added

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,8 @@ import {FaMapPin} from "react-icons/fa";
 import mypic from "../pictures/mypic.jpg"
 import Contact from "./contact/Contact";
 import Backdrop from "./contact/Backdrop";
-
+import SearchIcon from '@material-ui/icons/Search';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 
 export default function Main() {
 
@@ -20,20 +21,20 @@ export default function Main() {
     return (
       
         <div className="mainMain">
-            <div className="welcome text-center bg-blue-100 rounded-xl text-black-700">
-                <h1 className="h3">Welcome!</h1>
-                <p className="px-6 text-left leading-8 font-bold">
-                    The goal of this website is to introduce myself and share some experiences with people.
-                    <br/>
-                    I am a software developer with a few years of experience in front and back ends. Currently, 
-                    I am working as a software engineer at Su Bilgi Teknolojileri A.Ş.
-                    <br/>
-                    Here on this site, you can find some of my works, experiences, and mistakes.
-                    The site is not complete and it won't be any time soon.
-                    Well,it is not because I am lazy, it is because there will always be new things to learn and add to the site<>&#128513;</>.
-                </p>
-            </div>
-            <div className="myPicDiv bg-blue-100 rounded-xl">
+            <div className="div1 text-center rounded-xl bg-white">
+                <div className="myIntro">
+                    <h1 className="h3 text-left px-7">Welcome!</h1>
+                    <p className="px-6 text-left leading-8 ">
+                        The goal of this website is to introduce myself and share some experiences with people.
+                        <br />
+                        I am a software developer with a few years of experience in front and back ends. Currently,
+                        I am working as a software engineer at Su Bilgi Teknolojileri A.Ş.
+                        <br />
+                        Here on this site, you can find some of my works, experiences, and mistakes.
+                        The site is not complete and it won't be any time soon.
+                        Well,it is not because I am lazy, it is because there will always be new things to learn and add to the site <>&#128513;</>.
+                    </p>
+                </div>
                 <div className="myCard" onClick={showModal}>
                     <FaMapPin className="MainfaMapPin" />
                     <img src={mypic} alt="mypic" />
@@ -41,9 +42,35 @@ export default function Main() {
                     <p><em>Yee, this is me.</em></p>
                 </div>
             </div>
+
+            <div className="div5 rounded-xl">
+                <h3 className="h4 text-left pl-9 pt-2">Contact</h3>
+                <p className="text-right text-white pr-2">< SearchIcon className="searchIcon" onClick={showModal} /></p>
+            </div>
+            <div className="div4 rounded-xl">
+                <h3 className="h4 text-left pl-9 pt-2">Code Samples</h3>
+                <p className="text-right text-white pr-2">
+                    <Link to="/codes"> < SearchIcon className="searchIcon" /></Link>
+
+                </p>
+            </div>
+            <div className="div3 rounded-xl">
+                <h3 className="h4 text-left pl-9 pt-2">API Fetch Example</h3>
+
+                <p className="text-right text-white pr-2">
+                    <Link to="/fetch">< SearchIcon className="searchIcon" /></Link>
+                </p>
+            </div>
+            <div className="div2 rounded-xl">
+                <h3 className="h4 text-left pl-9 pt-2">Works</h3>
+                <p className="text-right text-white pr-2">
+                    <Link to="/repos">< SearchIcon className="searchIcon" /></Link>
+                </p>
+            </div>
+
             {show && <Backdrop onClick={closeModal} />}
             {show && <Contact onClick={closeModal} />}
         </div>
-    
+
     )
 }
